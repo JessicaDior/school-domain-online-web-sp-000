fdb3045887b20568ef6091feee56aac23e1d0a38
Jc Dior <jessicaclairdior@gmail.com>
2020-09-16 23:48:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  attr_reader :school_name, :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+def add_student(name, grade)
+  if @roster.include?(grade)
+  @roster[grade] << name
+else
+  @roster[grade] = [name]
+end
+end
+
+end
